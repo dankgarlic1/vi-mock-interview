495db144b4a1c6d48582aa654a59d7519cdbe360
Harshit Raizada <harshitraizada@Harshits-MacBook-Air.local>
2026-03-21 14:44:58 +0530

    migrating google sdk

--- src/lib/GeminiClient.ts
+++ src/lib/GeminiClient.ts
@@ -1,5 +1,6 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from '@google/genai';
 
-export const genAI = new GoogleGenerativeAI(
-    process.env.GEMINI_API_KEY || process.env.NEXT_PUBLIC_GEMINI_API_KEY || ""
-  );
+export const genAI = new GoogleGenAI({
+  apiKey:
+    process.env.GEMINI_API_KEY || process.env.NEXT_PUBLIC_GEMINI_API_KEY || '',
+});
